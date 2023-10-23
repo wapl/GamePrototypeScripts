7513150969fbe68e5cfba1a2e4ccc7adc65e4dcf
Wapl <josemartinroxas@gmail.com>
2023-10-23 23:18:39 +0800

    Add files via upload

---
+++ CluesActor.cpp
@@ -0,0 +1,97 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CluesActor.h"
+#include "GameFramework/Actor.h"
+#include "Kismet/GameplayStatics.h"
+#include "Runtime/UMG/Public/UMG.h"
+#include "Runtime/UMG/Public/UMGStyle.h"
+#include "Runtime/UMG/Public/Slate/SObjectWidget.h"
+#include "Runtime/UMG/Public/IUMGModule.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
+#include "Slate.h"
+
+// Sets default values
+ACluesActor::ACluesActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	clueMesh=CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	clueMesh->SetupAttachment(RootComponent);
+
+	clueMesh->SetRelativeLocation(FVector(0.0f,0.0f,0.0f));
+
+	clueCollider=CreateDefaultSubobject<UBoxComponent>(TEXT("Clue Collider") );
+	clueCollider->SetupAttachment(clueMesh);
+	clueCollider->SetRelativeLocation(FVector(0.0f,0.0f,0.0f));
+	clueCollider->SetRelativeScale3D(FVector(2.0f,2.0f,2.0f));
+
+	clueCollider->OnComponentBeginOverlap.AddDynamic(this,&ACluesActor::OnOverlapBegin);
+	clueCollider->OnComponentEndOverlap.AddDynamic(this,&ACluesActor::OnOverlapEnd);
+	//PC=UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	
+	
+	ConstructorHelpers::FClassFinder<UUserWidget>CluePromptBPClass(TEXT("/Game/blueprints/CluePrompt"));
+	cluePromptUI=CluePromptBPClass.Class;
+	
+	//InputComponent->BindKey(EKeys::E, IE_Pressed, this, &Contoller::AReleased);
+}
+
+// Called when the game starts or when spawned
+void ACluesActor::BeginPlay()
+{
+	Super::BeginPlay();
+	clueRef=CreateWidget<UUserWidget>(GetWorld(),cluePromptUI,"CluePrompt");
+	FName TestUiName = FName(TEXT("ClueText"));
+	
+	Cast<UTextBlock>(clueRef->GetWidgetFromName(TestUiName))->SetText(FText::FromString(clueText));
+	
+	PC=GetWorld()->GetFirstPlayerController();
+}
+
+// Called every frame
+void ACluesActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(PC!=nullptr)
+	{
+		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Player controller found"));
+		if(PC->WasInputKeyJustPressed(EKeys::E))
+		{
+			if(GEngine)
+				GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("E Pressed "));
+			
+			clueRef->AddToViewport();
+		}
+	
+	}
+	else
+	{
+		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Not Found"));
+	}
+	
+	
+	
+}
+void ACluesActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(GEngine)
+         GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Enter!"));	
+	
+	EnableInput(PC);
+}
+void ACluesActor::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if(GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Exit!"));	
+	DisableInput(PC);
+	clueRef->RemoveFromViewport();
+}
+
+
+
+
+
+
---
+++ CluesActor.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <string>
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "Runtime/UMG/Public/UMG.h"
+#include "Runtime/UMG/Public/UMGStyle.h"
+#include "Runtime/UMG/Public/Slate/SObjectWidget.h"
+#include "Runtime/UMG/Public/IUMGModule.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
+#include "Slate.h"
+#include "CluesActor.generated.h"
+
+
+UCLASS()
+class MYGAME_API ACluesActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACluesActor();
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* clueMesh;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="CluesText")
+	FString clueText;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Collider")
+	UBoxComponent* clueCollider;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	
+	
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	UPROPERTY()
+	APlayerController* PC;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="UI")
+	TSubclassOf<UUserWidget> cluePromptUI;
+
+	UPROPERTY()
+	UUserWidget* clueRef;
+
+	
+	
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
